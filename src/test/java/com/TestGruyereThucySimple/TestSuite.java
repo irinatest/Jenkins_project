d4e08c9a4d7eba47fdb29ef797b998630965620c
Iris <Iris@Iris-PC>
2015-02-16 22:03:34 +0200

    Updated pom and suite

---
+++ src/test/java/com/TestGruyereThucySimple/TestSuite.java
@@ -0,0 +1,13 @@
+package com.TestGruyereThucySimple;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@Suite.SuiteClasses({
+	LogInTest.class,
+	})
+@RunWith(Suite.class)
+public class TestSuite {
+
+}
